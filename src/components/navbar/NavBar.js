3229877347777a581061b88ecbcca12e13d8b139
ShenMoe84 <s.monroe.84@gmail.com>
2025-11-17 12:47:51 -0600

    begins work on hamburger menu for nav element

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
 
 const NavBar = () => {
+
+  const [isOpen, setOpen] =  useState(false)
 
   return (
     <nav className={styles.navBar}>
