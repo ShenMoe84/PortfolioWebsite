8c5233ea712d1fb32bf5e3ea54621d50a3748bfe
ShenMoe84 <s.monroe.84@gmail.com>
2025-11-19 12:55:35 -0600

    begins structure for hamburger nav menu

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 import { NavLink } from 'react-router-dom';
 import styles from './NavBar.module.css';
+import { Fade as Hamburger } from "hamburger-react";
 
 const NavBar = () => {
 
-  const [isOpen, setOpen] =  useState(false)
+  const [isOpen, setOpen] = useState(false)
+
+  const handleToggle = () => {
+
+  }
 
   return (
     <nav className={styles.navBar}>
-      <div>
+      <div className={styles.headerCont}>
         <NavLink to="/" className={styles.home}>Sheena Monroe</NavLink>
-        <h3 className={styles.tagLine}>Innovation Driven || Solution Focused || Creativity Enthusiast</h3>
+        <h3 className={styles.tagLine}>Innovation Driven | Solution Focused | Creativity Enthusiast</h3>
+      </div>
+      <div className={styles.navCont}>
         <NavLink to="/graduatework" className={({ isActive }) => isActive ? styles.active : styles.link}>Graduate Work</NavLink>
         <NavLink to="/softwaredevelopment" className={({ isActive }) => isActive ? styles.active : styles.link}>Software Development</NavLink>
         <NavLink to="/productdevelopment" className={({ isActive }) => isActive ? styles.active : styles.link}>Product Development</NavLink>
         <NavLink to="/more" className={({ isActive }) => isActive ? styles.active : styles.link}>More</NavLink>
       </div>
+      <Hamburger onToggle={setOpen}/>
     </nav>
   );
 };
